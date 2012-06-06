2c60ba34fe4d15ed837bab8be72c3cd286e65929
guokr <guokr@localhost>
2012-06-06 18:24:16 +0800

    add public

---
+++ public/js/test.js
@@ -0,0 +1,3 @@
+/* author: carl, date: Thu Jan 05 2012 18:35:59 GMT+0800 (CST) */Ext.require(["*"]);
+Ext.onReady(function(){Ext.create("Ext.data.Store",{storeId:"simpsonsStore",fields:["name","email","phone"],data:{items:[{name:"Lisa",email:"lisa@simpsons.com",phone:"555-111-1224"},{name:"Bart",email:"bart@simpsons.com",phone:"555-222-1234"},{name:"Homer",email:"home@simpsons.com",phone:"555-222-1244"},{name:"Marge",email:"marge@simpsons.com",phone:"555-222-1254"}]},proxy:{type:"memory",reader:{type:"json",root:"items"}}});Ext.create("Ext.grid.Panel",{title:"Simpsons",store:Ext.data.StoreManager.lookup("simpsonsStore"),columns:[{header:"Name",
+dataIndex:"name"},{header:"Email",dataIndex:"email",flex:1},{header:"Phone",dataIndex:"phone"}],height:200,width:400,renderTo:Ext.getBody()})});
